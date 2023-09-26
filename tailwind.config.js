522f6ed6a805d7aee862afa31c976a13fd4be08b
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-09-26 19:11:56 +0200

    Add logo files and update TailwindCSS configuration
    - Add logo-tm-official.png, logo-tm.ico, logo-tm.png, and task-manager.jpg files to the public/img directory.
    - Update the TailwindCSS configuration in tailwind.config.js to include specific HTML files.
    - Extend the theme colors in the TailwindCSS configuration to include light-blue, dark-blue, red-error, and light-grey.
    
    Note: The logo files have been added and the TailwindCSS configuration has been updated to include specific HTML files and extend the theme colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: [
+		'./src/home.html',
+		'./src/pages/dashboard.html',
+		'./src/pages/workspace.html',
+		'./src/pages/task.html',
+	],
+	theme: {
+		extend: {
+			colors: {
+				'light-blue': '#eaefff',
+				'dark-blue': '#171f39',
+				'red-error': '#e1786a',
+				'light-grey': '#a8a9b4',
+			},
+		},
+	},
+	plugins: [],
+};
