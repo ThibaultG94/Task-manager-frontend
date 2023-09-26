d686f2a5632a0ef425b2f54134deb0a33a3dfc60
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-09-26 21:00:39 +0200

    Adjust Home component layout and add login form.
    - Adjusted the layout of the Home component to use a grid system with columns taking 45% and 55% of the width.
    - Added a section with a centered flexbox container for the login form.
    - Created a form with an email input field and error message placeholder.

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,34 @@ import React from 'react';
 const Home = () => {
 	return (
 		<div className="flex items-center justify-center">
-			<h1>Home</h1>
+			<main className="min-w-full min-h-screen grid grid-cols-[45%_55%]">
+				<section className="flex justify-center px-20 py-10 bg-light-blue">
+					<div>
+						<h2 className="text-3xl my-10 text-center">
+							Connexion
+						</h2>
+						<form
+							id="login-form"
+							className="flex flex-col items-start mb-24">
+							<div className="flex flex-col">
+								<label for="email">Email</label>
+								<input
+									type="email"
+									id="emailLog"
+									name="email"
+									minlength="6"
+									maxlength="254"
+									autocomplete="off"
+									required
+								/>
+								<span
+									id="email-error"
+									className="error-message"></span>
+							</div>
+						</form>
+					</div>
+				</section>
+			</main>
 		</div>
 	);
 };
