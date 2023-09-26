ab1efe85d5475570d66dc7b4a11a026ce5d117e7
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-09-26 20:37:13 +0200

    Add react-router-dom to package dependencies
    - Adds react-router-dom version 6.16.0 to package.json and package-lock.json
    - Creates new file App.jsx to replace deleted file App.js
    - Updates import statements and uses BrowserRouter, Routes, and Route from react-router-dom
    - Adds new file Home.jsx for home page component
    - Renames src/index.js to src/index.jsx

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.scss';
+import Home from './Home';
+
+const App = () => {
+	return (
+		<div>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
+};
+
+export default App;
