19e69995ad35401af2f5143a10f172af03531dd7
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-10-06 23:58:29 +0200

    feat: Add dynamic calendar generation
    This commit adds functionality to dynamically generate calendar days based on the selected date. The current date and calendar days are managed using hooks and updated with the useEffect hook.

--- src/components/dashboard/Calendar.jsx
+++ src/components/dashboard/Calendar.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+const days = ['LUN', 'MAR', 'MER', 'JEU', 'VEN', 'SAM', 'DIM'];
+const months = [
+	'Janvier',
+	'Février',
+	'Mars',
+	'Avril',
+	'Mai',
+	'Juin',
+	'Juillet',
+	'Août',
+	'Septembre',
+	'Octobre',
+	'Novembre',
+	'Décembre',
+];
 
 const Calendar = () => {
+	const [currentDate, setCurrentDate] = useState(new Date());
+	const [calendarDays, setCalendarDays] = useState([]);
+
+	const generateDays = () => {};
+
+	useEffect(() => {
+		generateDays(currentDate);
+	}, [currentDate]);
+
 	return (
 		<div className="calendar-container dashboard-card flex justify-center items-center">
-			<div id="calendar" className="px-3 overflow-hidden"></div>
+			<div id="calendar" className="px-3 overflow-hidden">
+				<div className="month-year">
+					{`${
+						months[currentDate.getMonth()]
+					} ${currentDate.getFullYear()}`}
+				</div>
+				<div className="calendar-header"></div>
+			</div>
 		</div>
 	);
 };
