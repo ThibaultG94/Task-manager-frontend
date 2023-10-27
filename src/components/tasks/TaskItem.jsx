03408de4f236ea87c49278efe3019b5a95572708
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-10-27 13:31:18 +0200

    Refactor CSS styles for TaskItem component
    - The CSS styles for the TaskItem component have been refactored to improve readability and maintainability.
    - The styles for `.task-day`, `.task-status`, `.task-priority`, `.task-workspace`, and `.task-assigned` have been removed and replaced with new styles using the `text-left`, `mx-auto`, `p-1.5`, `px-2.5`, `rounded-lg`, and `.bg-[#171f39]` classes.
    - The new styles make use of flexbox and have a consistent appearance with rounded corners and appropriate spacing.
    - The `<span id="avatarLetterAssigned">` element now has a separate style applied to it using the `.text-left`, `.mx-auto`, `.p-1.5`, `.px-2.5`, `.bg-[#171f39]`, `.h-8`, `.w-8`, `.overflow-hidden`, `.cursor-pointer`, `.flex`, `.items-center`, `.justify-center`, and `.rounded-full` classes.

--- src/components/tasks/TaskItem.jsx
+++ src/components/tasks/TaskItem.jsx
@@ -13,19 +13,32 @@ const TaskItem = ({ task, openModal, setSelectedTask }) => {
 				<div className="rounded-full border border-black h-5 w-5 mr-2 bg-white self-center"></div>
 				{task.title}
 			</div>
-			<div className={`task-day ` + task.category}>
+			<div
+				className={
+					`text-left mx-auto p-1.5 px-2.5 rounded-lg ` + task.category
+				}>
 				{task.status === 'Archived'
 					? inverseDateFormat(task.deadline)
 					: task.day}
 			</div>
-			<div className={`task-status ` + task.convertedStatus}>
+			<div
+				className={
+					`text-left mx-auto p-1.5 px-2.5 rounded-lg ` +
+					task.convertedStatus
+				}>
 				{task.convertedStatus}
 			</div>
-			<div className={`task-priority ` + task.convertedPriority}>
+			<div
+				className={
+					`text-left mx-auto p-1.5 px-2.5 rounded-lg ` +
+					task.convertedPriority
+				}>
 				{task.convertedPriority}
 			</div>
-			<div className="task-workspace">{task.workspaceTitle}</div>
-			<div className="task-assigned">
+			<div className="text-left mx-auto p-1.5 px-2.5 rounded-lg">
+				{task.workspaceTitle}
+			</div>
+			<div className="text-left mx-auto p-1.5 px-2.5 bg-[#171f39] h-8 w-8 overflow-hidden cursor-pointer flex items-center justify-center rounded-full">
 				<span id="avatarLetterAssigned">{task.assignedToLetter}</span>
 			</div>
 		</div>
