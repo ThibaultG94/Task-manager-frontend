7472657c92d14a377b71d73b8f786d92c2cf9ac8
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-10-31 18:14:14 +0100

    Refactor Dashboard.jsx for improved code readability and cleanliness.
    - Remove unnecessary padding from the container div.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -68,7 +68,7 @@ const Dashboard = () => {
 			<section className="bg-dark-blue text-white text-center">
 				<SideBar userId={userId} />
 			</section>
-			<div className="w-full p-2.5 bg-light-blue">
+			<div className="w-full bg-light-blue">
 				<Header />
 				<main className="dashboard-container">
 					<Calendar />
