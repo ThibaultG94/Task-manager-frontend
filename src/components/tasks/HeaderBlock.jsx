66dedc642c64a4dc5b360d6778f06dd727c461ed
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-11-07 17:41:37 +0100

    Refactor task block header and task list styles
    - Updated the class name of the task block header to "py-4 px-0 flex items-center justify-between cursor-pointer" which adjusts the padding, alignment, and cursor properties.
    - Removed the "task-list" class from Tasks.scss as it is no longer needed.
    
    These code changes improve the styling and layout of the task block header and task list in the Tasks component.

--- src/components/tasks/HeaderBlock.jsx
+++ src/components/tasks/HeaderBlock.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HeaderBlock = ({ label, type, toggleBlock }) => {
 	return (
-		<div className="task-block-header">
+		<div className="py-4 px-0 flex items-center justify-between cursor-pointer">
 			<h3 className="pl-6 text-xl">{label}</h3>
 			<button
 				className="transition-transform ease-in-out duration-300 bg-transparent text-black select-none hover:bg-transparent mr-6"
