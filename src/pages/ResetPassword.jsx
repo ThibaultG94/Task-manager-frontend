62718f191f16de4af24fd01f52eae4737dd88016
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-11-15 11:25:36 +0100

    Update password validation and display errors
    - Added state for password confirmation and errors
    - Implemented a password validation function to check if the passwords match and have a minimum length of 6 characters
    - Updated the submit function to call the password validation function before resetting the password
    - Displayed error messages for password and password confirmation fields
    - Updated the UI to include labels and styles for password and password confirmation fields

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -5,41 +5,111 @@ import { toast } from 'react-toastify';
 
 const ResetPassword = () => {
 	const [password, setPassword] = useState('');
+	const [passwordConfirm, setPasswordConfirm] = useState('');
 	const navigate = useNavigate();
 	const { token } = useParams();
 	const resetPassword = useResetPassword();
 
+	const [errors, setErrors] = useState({
+		password: null,
+		passwordConfirm: null,
+	});
+
+	const validatePasswords = () => {
+		let tempErrors = { ...errors };
+		let isValid = true;
+
+		if (password !== passwordConfirm) {
+			tempErrors.passwordConfirm =
+				'Les mots de passe ne correspondent pas';
+			isValid = false;
+		}
+
+		if (password.length < 6) {
+			tempErrors.password =
+				'Le mot de passe doit contenir au moins 6 caractères';
+			isValid = false;
+		}
+
+		setErrors(tempErrors);
+		return isValid;
+	};
+
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 
-		try {
-			await resetPassword(token, password);
-			navigate('/');
-			toast.success('Votre mot de passe a bien été réinitialisé');
-		} catch (error) {
-			console.error('Password reset error', error);
-			toast.error(
-				'Une erreur est survenue lors de la réinitialisation du mot de passe'
-			);
+		if (validatePasswords()) {
+			try {
+				await resetPassword(token, password);
+				navigate('/');
+				toast.success('Votre mot de passe a bien été réinitialisé');
+			} catch (error) {
+				console.error('Password reset error', error);
+				toast.error(
+					'Une erreur est sur	venue lors de la réinitialisation du mot de passe'
+				);
+			}
 		}
 	};
 
 	return (
-		<div>
-			<form onSubmit={handleSubmit}>
-				<input
-					autoComplete="off"
-					minLength="6"
-					maxLength="254"
-					onChange={(e) => setPassword(e.target.value)}
-					placeholder="Nouveau mot de passe"
-					required
-					type="password"
-					value={password}
-				/>
-				<br />
-				<button type="submit">Réinitialiser le mot de passe</button>
-			</form>
+		<div className="bg-light-blue flex items-center justify-center min-h-screen">
+			<div className="bg-white max-w-lg p-8 rounded-lg shadow-md space-y-4 w-full">
+				<h2 className="font-medium text-3xl text-dark-blue">
+					Réinitialiser le mot de passe
+				</h2>
+				<form className="space-y-2" onSubmit={handleSubmit}>
+					<div>
+						<label
+							className="block text-md text-gray-800"
+							htmlFor="password">
+							Nouveau mot de passe
+						</label>
+						<input
+							autoComplete="off"
+							className="appearance-none border focus:border-blue-300 h-9 mt-2 px-2 rounded-lg shadow text-base transition-colors w-full"
+							id="password"
+							minLength="6"
+							maxLength="254"
+							onChange={(e) => setPassword(e.target.value)}
+							required
+							type="password"
+							value={password}
+						/>
+						<span className="h-9 my-1 text-red-400 text-sm max-w-[350px]">
+							{errors.password}
+						</span>
+					</div>
+					<div>
+						<label
+							className="block text-md text-gray-800"
+							htmlFor="passwordConfirm">
+							Confirmer le mot de passe
+						</label>
+						<input
+							autoComplete="off"
+							className="appearance-none border focus:border-blue-300 h-9 mt-2 px-2 rounded-lg shadow text-base transition-colors w-full"
+							id="passwordConfirm"
+							minLength="6"
+							maxLength="254"
+							onChange={(e) => setPasswordConfirm(e.target.value)}
+							required
+							type="password"
+							value={passwordConfirm}
+						/>
+						<span className="h-9 my-1 text-red-400 text-sm max-w-[350px]">
+							{errors.passwordConfirm}
+						</span>
+					</div>
+					<div className="flex justify-end">
+						<button
+							className="bg-dark-blue hover:bg-dark-purple mt-2 px-4 py-2 rounded-lg text-white"
+							type="submit">
+							Mettre à jour le mot de passe
+						</button>
+					</div>
+				</form>
+			</div>
 		</div>
 	);
 };
