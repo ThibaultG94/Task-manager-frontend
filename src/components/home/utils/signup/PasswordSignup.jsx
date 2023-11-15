3bae06c5a2a86436803e5778aa6dafa7d30fee68
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-11-15 14:15:54 +0100

    Update PasswordSignup component to include password strength evaluation and a progress bar.
    - Set up state variable `passwordStrength` to keep track of password strength.
    - Implement `evaluatePasswordStrength` function to calculate the strength based on password criteria.
    - Use `useEffect` to update `passwordStrength` whenever the password in the form data changes.
    - Add `progressBarColor` method to determine the color of the progress bar based on the password strength.
    - Replace the commented out code with a new progress bar that reflects the updated password strength.
    - Update the invalid password error message to display any errors related to the password.

--- src/components/home/utils/signup/PasswordSignup.jsx
+++ src/components/home/utils/signup/PasswordSignup.jsx
@@ -1,6 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const PasswordSignup = ({ errors, formData, handleChange, progressBar }) => {
+	const [passwordStrength, setPasswordStrength] = useState(0);
+
+	const evaluatePasswordStrength = (password) => {
+		let strength = 0;
+		const regex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[\d])(?=.*?[\W]).{8,}$/;
+
+		if (password.length >= 8) strength += 25;
+		if (password.match(/[a-z]+/)) strength += 25;
+		if (password.match(/[A-Z]+/)) strength += 25;
+		if (password.match(/[0-9]+/)) strength += 25;
+		if (password.match(/[\W]+/)) strength += 25;
+
+		if (!password.match(regex)) {
+			strength -= 50;
+		}
+
+		return Math.max(Math.min(strength, 100), 0);
+	};
+
+	useEffect(() => {
+		setPasswordStrength(evaluatePasswordStrength(formData.password));
+	}, [formData.password]);
+
+	const progressBarColor = (strength) => {
+		if (strength < 50) return 'bg-red-500';
+		if (strength < 75) return 'bg-yellow-500';
+		if (strength < 100) return 'bg-blue-500';
+		return 'bg-green-500';
+	};
+
 	return (
 		<div className="flex flex-col">
 			<label className="block text-md text-gray-800" htmlFor="password">
@@ -23,21 +53,16 @@ const PasswordSignup = ({ errors, formData, handleChange, progressBar }) => {
 				value={formData.password}
 			/>
 			<span className={progressBar}></span>
-			{/* {isTypingPassword && (
-							<div className="relative pt-1">
-								<div className="overflow-hidden h-2 mb-4 text-xs flex rounded bg-red-200">
-									<div
-										style={{ width: '30%' }}
-										className="h-1 bg-red-500"></div>
-									<div
-										style={{ width: '60%' }}
-										className="h-1 bg-yellow-500"></div>
-									<div
-										style={{ width: '100%' }}
-										className="h-1 bg-green-500"></div>
-								</div>
-							</div>
-						)} */}
+
+			<div className="bg-gray-200 h-2.5 mt-2 rounded-full w-full">
+				<div
+					className={
+						progressBarColor(passwordStrength) +
+						' h-2.5 rounded-full'
+					}
+					style={{ width: `${passwordStrength}%` }}></div>
+			</div>
+
 			<span className="h-9 my-1 text-red-400 text-sm max-w-[350px]">
 				{errors.password}
 			</span>
