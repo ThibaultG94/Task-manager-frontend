ec2e40688753b4df4ef6f1ffa0a02c7ce0881f5d
Thibault Guilhem <thibault.guilhem@gmail.com>
2023-12-29 15:15:36 +0100

    Refine pagination responsiveness and simplify click event
    Streamlined the pagination component by removing redundant click event handlers and improving responsiveness across different screen sizes. By employing `flex-wrap` and adjusting padding and margin, the pagination adapts seamlessly from mobile to desktop views. Text sizes now adjust with breakpoints, ensuring readability. The simplified click handler enhances overall performance and code clarity.

--- src/components/tasks/Pagination.jsx
+++ src/components/tasks/Pagination.jsx
@@ -6,24 +6,19 @@ const Pagination = ({ currentPage, setPage, totalPages }) => {
 	};
 
 	return (
-		<div className="flex items-center justify-center space-x-1 pb-4">
+		<div className="flex flex-wrap items-center justify-center space-x-1 pb-2 sm:pb-4">
 			{Array.from({ length: totalPages }, (_, index) => index + 1).map(
 				(page) => (
 					<div
 						key={page}
-						role="button"
-						tabIndex={0}
-						onClick={(e) => {
-							e.stopPropagation();
-							setPage(page);
-						}}
-						onKeyDown={(event) => handleKeyDown(event, page)}
-						aria-label={`Go to page ${page}`}
-						className={`cursor-pointer px-4 py-2 text-sm font-medium rounded-md transition-colors duration-200 ${
+						className={`cursor-pointer px-4 py-2 text-xs sm:text-sm font-medium rounded-md transition-colors duration-200 ${
 							currentPage === page
 								? 'bg-orange-primary text-white'
 								: 'bg-white text-gray-700 hover:bg-pink-primary'
-						}`}>
+						} sm:inline-block sm:mr-2`}
+						onClick={() => setPage(page)}
+						onKeyDown={(event) => handleKeyDown(event, page)}
+						aria-label={`Go to page ${page}`}>
 						{page}
 					</div>
 				)
