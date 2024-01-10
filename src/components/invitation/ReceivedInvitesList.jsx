31f7f2b9b3b6d67e35fdfa70d1d5e5d008f61582
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-01-10 18:37:57 +0100

    Enhanced invite visualization with actions
    Reconfigured the ReceivedInvitesList component for a cleaner UI by adding action buttons that allow users to accept or decline invites directly from the list. Improved the layout by arranging content and actions side by side and styling them for responsive behavior. Hover and focus states for the action buttons were added for better visual feedback, with color-coded icons for immediate recognition of the action – green for accept and red for decline. Updated associated styles to ensure a consistent and accessible user experience across different screen sizes.

--- src/components/invitation/ReceivedInvitesList.jsx
+++ src/components/invitation/ReceivedInvitesList.jsx
@@ -33,15 +33,51 @@ const ReceivedInvitesList = ({ userId }) => {
 							<div
 								key={invitation.invitationId}
 								className="bg-light-blue rounded-lg p-4 mb-4 last:mb-0 hover:bg-blue-200 transition duration-300 ease-in-out">
-								<p className="text-dark-blue font-medium">
-									{invitation.senderUsername}{' '}
-									<span className="text-gray-500">
-										({invitation.senderEmail})
-									</span>
-								</p>
-								<p className="text-gray-600 italic">
-									{invitation.message}
-								</p>
+								<div className="flex justify-between items-center">
+									<div>
+										<p className="text-dark-blue font-medium">
+											{invitation.senderUsername}{' '}
+											<span className="text-gray-500">
+												({invitation.senderEmail})
+											</span>
+										</p>
+										<p className="text-gray-600 italic">
+											{invitation.message}
+										</p>
+									</div>
+									<div className="flex flex-col gap-2 ml-2 invitation-list">
+										<button className="accept-icon">
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												fill="none"
+												viewBox="0 0 24 24"
+												strokeWidth={1.5}
+												stroke="currentColor"
+												className="w-6 h-6">
+												<path
+													strokeLinecap="round"
+													strokeLinejoin="round"
+													d="M4.5 12.75l6 6 9-13.5"
+												/>
+											</svg>
+										</button>
+										<button className="decline-icon">
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												fill="none"
+												viewBox="0 0 24 24"
+												strokeWidth={1.5}
+												stroke="currentColor"
+												className="w-6 h-6">
+												<path
+													strokeLinecap="round"
+													strokeLinejoin="round"
+													d="M6 18L18 6M6 6l12 12"
+												/>
+											</svg>
+										</button>
+									</div>
+								</div>
 							</div>
 						))}
 					{receivedInvitationsPending &&
