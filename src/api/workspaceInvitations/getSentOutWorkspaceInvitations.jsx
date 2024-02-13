f636acb1cc5647affa88f30dae2dd52645b6ad3d
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-02-13 16:15:36 +0100

    Add useGetSentOutWorkspaceInvitations hook to fetch sent out workspace invitations

---
+++ src/api/workspaceInvitations/getSentOutWorkspaceInvitations.jsx
@@ -0,0 +1,38 @@
+import { useDispatch } from 'react-redux';
+import { useErrorApi } from '../../components/utils/ErrorApi';
+import {
+	setSendOutWorkspaceInvitationsAction,
+	setSendOutWorkspaceInvitationsSuccess,
+	setSendOutWorkspaceInvitationsFailure,
+} from '../../store/feature/workspaceInvitations.slice';
+import axios from 'axios';
+
+export const useGetSentOutWorkspaceInvitations = () => {
+	const dispatch = useDispatch();
+	const errorApi = useErrorApi();
+
+	const getSendOutWorkspaceInvitations = async (userId) => {
+		dispatch(setSendOutWorkspaceInvitationsAction());
+
+		try {
+			const API_URL = process.env.REACT_APP_API_URL;
+			const res = await axios.get(
+				`${API_URL}/workspaceInvitations/sentout-invitations/${userId}`,
+				{
+					withCredentials: true,
+				}
+			);
+			dispatch(
+				setSendOutWorkspaceInvitationsSuccess(
+					res.data.workspaceInvitations
+				)
+			);
+			return res.data.workspaceInvitations;
+		} catch (error) {
+			dispatch(setSendOutWorkspaceInvitationsFailure(error));
+			errorApi(error);
+		}
+	};
+
+	return getSendOutWorkspaceInvitations;
+};
