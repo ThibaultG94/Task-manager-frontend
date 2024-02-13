c5631c0f907fc80ee79eeb55bc38506134d37799
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-02-13 16:06:16 +0100

    Add sendWorkspaceInvitationAction and implement sendInvitationWorkspace function

--- src/api/workspaceInvitations/sendInvitationWorkspace.jsx
+++ src/api/workspaceInvitations/sendInvitationWorkspace.jsx
@@ -1,10 +1,28 @@
 import { useDispatch } from 'react-redux';
 import axios from 'axios';
+import { sendWorkspaceInvitationAction } from '../../store/feature/workspaceInvitation.slice';
 
 export const useSendInvitationWorkspace = () => {
 	const dispatch = useDispatch();
 
-	const sendInvitationWorkspace = async (invitation) => {};
+	const sendInvitationWorkspace = async (invitation) => {
+		dispatch(sendWorkspaceInvitationAction());
+
+		const API_URL = process.env.REACT_APP_API_URL;
+		const res = await axios.post(
+			`${API_URL}/workspaceInvitations/send-invitation/`,
+			{
+				senderId: invitation.userId,
+				guestId: invitation.contactId,
+				role: invitation.role,
+				workspaceId: invitation.workspaceId,
+			},
+			{
+				withCredentials: true,
+			}
+		);
+		return res;
+	};
 
 	return sendInvitationWorkspace;
 };
