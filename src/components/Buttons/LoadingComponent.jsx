691fbce580f859593fe7362f7a6b2f72a4bfef1d
Thibault <thibault.guilhem@gmail.com>
2024-04-26 22:26:36 +0200

    Add LoadingComponent

---
+++ src/components/Buttons/LoadingComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingComponent = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <div className="loader"></div>
+        </div>
+    );
+};
+
+export default LoadingComponent;
