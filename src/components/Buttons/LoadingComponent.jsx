95d53fb7fb4f410c92f219ef81cbef0ff48ddc1b
Thibault <thibault.guilhem@gmail.com>
2024-04-27 16:22:03 +0200

    Add LoadingComponent in urgentTasks

--- src/components/Buttons/LoadingComponent.jsx
+++ src/components/Buttons/LoadingComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadingComponent = () => {
     return (
-        <div className="flex justify-center items-center h-screen">
+        <div className="h-5/6 flex justify-center items-center">
             <div className="loader"></div>
         </div>
     );
--- src/components/DashboardPage/UrgentTasks.jsx
+++ src/components/DashboardPage/UrgentTasks.jsx
@@ -14,6 +14,7 @@ import { toast } from 'react-toastify';
 import { formatTaskForEditing } from '../../utils/formatTaskForEditing';
 import HandleModalTask from '../ModalTask/HandleModalTask';
 import { useGetWorkspace } from '../../api/workspaces/useGetWorkspace';
+import LoadingComponent from '../Buttons/LoadingComponent';
 
 const UrgentTasks = () => {
 	const dispatch = useDispatch();
@@ -28,6 +29,7 @@ const UrgentTasks = () => {
 	const [selectedTask, setSelectedTask] = useState(null);
 	const [isModalOpen, setIsModalOpen] = useState(false);
 	const [isEditing, setIsEditing] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
 
 	const checkIfEdited = useCheckIfEdited({
 		setIsModalOpen,
@@ -91,6 +93,7 @@ const UrgentTasks = () => {
 
 	useEffect(() => {
 		const updateDisplayTasks = async () => {
+			setIsLoading(true);
 			const updatedTasks = [];
 			for (let i = 0; i < 4; i++) {
 				if (urgentTasks && urgentTasks[i]) {
@@ -135,6 +138,7 @@ const UrgentTasks = () => {
 		};
 
 		updateDisplayTasks();
+		setIsLoading(false);
 	}, [urgentTasks]);
 
 	useEffect(() => {
@@ -150,59 +154,63 @@ const UrgentTasks = () => {
 	return (
 		<div className="dashboard-card tasks-container">
 			<h4 className="pl-2 sm:pl-3 md:pl-4">Tâches urgentes</h4>
-			<div className="flex flex-col">
-				{displayTasks && displayTasks?.length > 0 ? (
-					displayTasks.map((task, index) => (
-						<div
-							className={`urgent-task ${
-								task?.isOverdue ? 'task-overdue' : ''
-							}`}
-							key={index}
-							onClick={(e) => {
-								openModal(e);
-								setSelectedTask(task);
-							}}>
-							<div className="urgent-task-todo">
-								<div id="check"></div>
-								<div className="text-sm sm:text-base">
-									{task?.title}
-								</div>
-							</div>
-							<div className="flex flex-row">
-								<div className="mr-10 hidden sm:block">
-									<div className="font-medium text-dark-purple-2 text-xs sm:text-sm md:text-base">
-										{task?.workspaceName}
+			{isLoading ? (
+          			<LoadingComponent />
+			) : (
+				<div className="flex flex-col">
+					{displayTasks && displayTasks?.length > 0 ? (
+						displayTasks.map((task, index) => (
+							<div
+								className={`urgent-task ${
+									task?.isOverdue ? 'task-overdue' : ''
+								}`}
+								key={index}
+								onClick={(e) => {
+									openModal(e);
+									setSelectedTask(task);
+								}}>
+								<div className="urgent-task-todo">
+									<div id="check"></div>
+									<div className="text-sm sm:text-base">
+										{task?.title}
 									</div>
 								</div>
-								<div className="date-container">
-									<div className="date">{task?.date}</div>
+								<div className="flex flex-row">
+									<div className="mr-10 hidden sm:block">
+										<div className="font-medium text-dark-purple-2 text-xs sm:text-sm md:text-base">
+											{task?.workspaceName}
+										</div>
+									</div>
+									<div className="date-container">
+										<div className="date">{task?.date}</div>
+									</div>
+								</div>
+								<div
+									className="archive-icon archive-icon-urgent"
+									onClick={(e) => validateTask(e, task)}>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										fill="none"
+										viewBox="0 0 24 24"
+										strokeWidth={1.5}
+										stroke="currentColor"
+										className="w-6 h-6">
+										<path
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											d="M4.5 12.75l6 6 9-13.5"
+										/>
+									</svg>
 								</div>
 							</div>
-							<div
-								className="archive-icon archive-icon-urgent"
-								onClick={(e) => validateTask(e, task)}>
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									strokeWidth={1.5}
-									stroke="currentColor"
-									className="w-6 h-6">
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										d="M4.5 12.75l6 6 9-13.5"
-									/>
-								</svg>
-							</div>
+						))
+					) : (
+						<div className="no-urgent-tasks">
+							<p>Aucune tâche à afficher</p>
 						</div>
-					))
-				) : (
-					<div className="no-urgent-tasks">
-						<p>Aucune tâche à afficher</p>
-					</div>
-				)}
-			</div>
+					)}
+				</div>
+			)}
 
 			{isModalOpen && (
 				<HandleModalTask
