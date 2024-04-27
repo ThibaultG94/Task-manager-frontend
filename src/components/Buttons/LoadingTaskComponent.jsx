9f1cd91d87849effee72093d0311fe4c3836383e
Thibault <thibault.guilhem@gmail.com>
2024-04-27 17:59:55 +0200

    Loading overdue + today tasks

---
+++ src/components/Buttons/LoadingTaskComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingTaskComponent = () => {
+    return (
+        <div className="h-14 flex justify-center items-start">
+            <div className="loader-task"></div>
+        </div>
+    );
+};
+
+export default LoadingTaskComponent;
