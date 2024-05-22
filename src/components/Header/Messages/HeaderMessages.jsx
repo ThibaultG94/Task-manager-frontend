ae6a5c888b7416d1a6e58c7d02a04c95e1cc3c00
Thibault <thibault.guilhem@gmail.com>
2024-05-22 19:40:57 +0200

    chore: Update HeaderMessages and NotificationsMenu components

--- src/components/Header/Messages/HeaderMessages.jsx
+++ src/components/Header/Messages/HeaderMessages.jsx
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const HeaderMessages = () => {
+    const [showMessages, setShowMessages] = useState(false);
+    const [isClosing, setIsClosing] = useState(false);
+    const [unreadMessages, setUnreadMessages] = useState([]);
+    const [hasNewMessage, setHasNewMessage] = useState(0);
+    
+    const messageRef = useRef(null);
+    const showMessagesRef = useRef(showMessages);
+
+    const markAsViewed = async (messagesIds) => {
+		// await markNotificationsAsViewed(userId, messagesIds);
+	};
+
+    const closeHandler = () => {
+        setIsClosing(true);
+        setTimeout(() => {
+			setShowMessages(false);
+        }, 300);
+    };
+
+    const handleMessagesMenu = (event) => {
+		event.stopPropagation();
+		if (!showMessagesRef.current) {
+			setIsClosing(false);
+			setShowMessages(true);
+			const viewedMessagesIds = unreadMessages
+				.filter((notif) => !notif.viewedAt)
+				.map((notif) => notif._id);
+				if (viewedMessagesIds.length > 0) {
+					markAsViewed(viewedMessagesIds);
+				}
+		} else {
+			closeHandler();
+		}
+	};
+
+    useEffect(() => {
+		showMessagesRef.current = showMessages;
+	}, [showMessages]);
+
     return (
-        <div className="flex relative mr-3 sm:mr-5 h-8 sm:h-10 md:h-12 mt-2 md:mt-0 items-center justify-center">
+        <div className="flex relative mr-3 sm:mr-5 h-8 sm:h-10 md:h-12 mt-2 md:mt-0 items-center justify-center" onClick={(e) => handleMessagesMenu(e)} 
+        ref={messageRef}>
             <span className="cursor-pointer text-dark-blue text-2xl sm:text-3xl">
                 <i className="fa-regular fa-envelope"></i>
 			</span>
+            {hasNewMessage > 0 && (
+				<span className="absolute top-0 sm:top-1 md:top-0 md:bottom-5 left-3.5 sm:left-4 h-4 sm:h-5 md:h-6 w-4 sm:w-5 md:w-6 bg-red-500 rounded-full flex items-center justify-center">
+					<span className="text-white text-[0.5rem] sm:text-xs font-semibold flex items-center justify-center">
+						{hasNewMessage < 100 ? hasNewMessage : '99+'}
+					</span>
+				</span>
+			)}
         </div>
     );
 };
--- src/components/Header/Notifications/HeaderNotifications.jsx
+++ src/components/Header/Notifications/HeaderNotifications.jsx
@@ -247,9 +247,9 @@ const HeaderNotifications = ({ userId }) => {
 				<i className="fa-regular fa-bell"></i>
 			</span>
 			{hasNewNotification > 0 && (
-				<span className="absolute top-0.5 sm:top-1 left-3 sm:left-4	h-3 sm:h-4 md:h-4 w-3 sm:w-4 md:w-4 bg-red-500 rounded-full flex items-center justify-center">
-					<span className="text-white text-[0.6rem] sm:text-xs font-semibold flex items-center justify-center">
-						{hasNewNotification}
+				<span className="absolute top-0 sm:top-1 md:top-0 md:bottom-5 left-3.5 sm:left-4 h-4 sm:h-5 md:h-6 w-4 sm:w-5 md:w-6 bg-red-500 rounded-full flex items-center justify-center">
+					<span className="text-white text-[0.5rem] sm:text-xs font-semibold flex items-center justify-center">
+					{hasNewNotification < 100 ? hasNewNotification : '99+'}
 					</span>
 				</span>
 			)}
