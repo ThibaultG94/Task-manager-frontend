03908c2cda862e5d4bd153525747126af0f26302
Thibault <thibault.guilhem@gmail.com>
2024-05-27 11:26:40 +0200

    chore: Update global.scss with slideDownNotif and slideUpNotif animations

--- src/components/Header/Messages/HeaderMessages.jsx
+++ src/components/Header/Messages/HeaderMessages.jsx
@@ -8,12 +8,11 @@ import { selectConversations } from '../../../store/selectors/conversationsSelec
 import getUserId from '../../../api/users/getUserId';
 
 const HeaderMessages = ({ userId }) => {
-	const getConversations = useGetConversations();
+    const getConversations = useGetConversations();
     const dispatch = useDispatch();
     const conversations = useSelector(selectConversations);
 
     const [showMessages, setShowMessages] = useState(false);
-    const [isClosing, setIsClosing] = useState(false);
     const [sortedConversations, setSortedConversations] = useState([]);
     const [unreadCount, setUnreadCount] = useState(0);
 
@@ -21,71 +20,57 @@ const HeaderMessages = ({ userId }) => {
     const messageRef = useRef(null);
     const showMessagesRef = useRef(showMessages);
 
-	const onOutsideClick = useCallback(() => {
-        closeHandler();
+    const onOutsideClick = useCallback(() => {
+        setShowMessages(false);
     }, []);
 
     useOutsideClick(modalRef, messageRef, onOutsideClick, showMessages);
 
     const markAsRead = async (conversationId) => {
-		dispatch(markConversationAsRead(conversationId));
-	};
-
-    const closeHandler = () => {
-        setIsClosing(true);
-        setTimeout(() => {
-			setShowMessages(false);
-        }, 600);
+        dispatch(markConversationAsRead(conversationId));
     };
 
     const handleMessagesMenu = (event) => {
-		event.stopPropagation();
-		if (!showMessagesRef.current) {
-			setIsClosing(false);
-			setShowMessages(true);
-		} else {
-			closeHandler();
-		}
-	};
+        event.stopPropagation();
+        setShowMessages(prevState => !prevState);
+    };
 
-	useEffect(() => {
-		getConversations();
-	}, []);
+    useEffect(() => {
+        getConversations();
+    }, []);
 
-	useEffect(() => {
-		if (conversations.length > 0) {
-			const sortedConvs = conversations
-				.filter(conv => conv.messages.length > 0)
-				.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt));
-			setSortedConversations(sortedConvs);
+    useEffect(() => {
+        if (conversations.length > 0) {
+            const sortedConvs = conversations
+                .filter(conv => conv.messages.length > 0)
+                .sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt));
+            setSortedConversations(sortedConvs);
 
-			const unreadMsgCount = sortedConvs.reduce((count, conv) => 
-				count + conv.messages.filter(msg => !msg.read && msg.guestId === userId).length, 0
-			);
-			setUnreadCount(unreadMsgCount);
-		}
-	}, [conversations]);
+            const unreadMsgCount = sortedConvs.reduce((count, conv) =>
+                count + conv.messages.filter(msg => !msg.read && msg.guestId === userId).length, 0
+            );
+            setUnreadCount(unreadMsgCount);
+        }
+    }, [conversations]);
 
-	useEffect(() => {
-		showMessagesRef.current = showMessages;
-	}, [showMessages]);
+    useEffect(() => {
+        showMessagesRef.current = showMessages;
+    }, [showMessages]);
 
     return (
         <div className="flex relative mr-3 sm:mr-5 h-8 sm:h-10 md:h-12 mt-2 md:mt-0 items-center justify-center" onClick={(e) => handleMessagesMenu(e)} 
         ref={messageRef}>
             <span className="cursor-pointer text-dark-blue text-2xl sm:text-3xl">
                 <i className="fa-regular fa-envelope"></i>
-			</span>
+            </span>
             {unreadCount > 0 && (
-				<span className="absolute top-0 sm:top-1 md:top-0 md:bottom-5 left-3.5 sm:left-4 h-4 sm:h-5 md:h-6 w-4 sm:w-5 md:w-6 bg-red-500 rounded-full flex items-center justify-center">
-					<span className="text-white text-[0.5rem] sm:text-xs font-semibold flex items-center justify-center">
-						{unreadCount < 100 ? unreadCount : '99+'}
-					</span>
-				</span>
-			)}
-			{showMessages && (
-				<MessagesMenu modalRef={modalRef} conversations={sortedConversations} onRead={markAsRead} userId={userId} isClosing={isClosing} />
-			)}
+                <span className="absolute top-0 sm:top-1 md:top-0 md:bottom-5 left-3.5 sm:left-4 h-4 sm:h-5 md:h-6 w-4 sm:w-5 md:w-6 bg-red-500 rounded-full flex items-center justify-center">
+                    <span className="text-white text-[0.5rem] sm:text-xs font-semibold flex items-center justify-center">
+                        {unreadCount < 100 ? unreadCount : '99+'}
+                    </span>
+                </span>
+            )}
+            <MessagesMenu modalRef={modalRef} conversations={sortedConversations} onRead={markAsRead} userId={userId} showMessages={showMessages} />
         </div>
     );
 };
--- src/components/Header/Messages/MessagesMenu.jsx
+++ src/components/Header/Messages/MessagesMenu.jsx
@@ -2,68 +2,72 @@ import React, { useEffect } from 'react';
 import { formatDistanceToNow, parseISO } from 'date-fns';
 import { fr } from 'date-fns/locale';
 
-const MessagesMenu = ({ modalRef, conversations, onRead, userId, isClosing }) => {
+const MessagesMenu = ({ modalRef, conversations, onRead, userId, showMessages }) => {
     const formatDateToNow = (dateString) => {
-		return formatDistanceToNow(parseISO(dateString), {
-			addSuffix: true,
-			locale: fr,
-		});
-	};
+        return formatDistanceToNow(parseISO(dateString), {
+            addSuffix: true,
+            locale: fr,
+        });
+    };
 
-	useEffect(() => {
+    useEffect(() => {
         const currentModal = modalRef.current;
         if (currentModal) {
-            currentModal.style.animation = `${isClosing ? 'slideUpNotif' : 'slideDownNotif'} 0.3s forwards`;
+            if (showMessages) {
+                currentModal.classList.add('slideDownNotif');
+                currentModal.classList.remove('slideUpNotif');
+            } else {
+                currentModal.classList.add('slideUpNotif');
+                currentModal.classList.remove('slideDownNotif');
+            }
         }
-    }, [isClosing]);
+    }, [showMessages]);
 
     return (
         <div
-			className="absolute top-full right-0 w-80 bg-white rounded-md shadow-lg border-gray-200 z-10 overflow-hidden pt-2 select-none"
-			onClick={(e) => e.stopPropagation()} ref={modalRef}>
+            className={`absolute top-full right-0 w-80 bg-white rounded-md shadow-lg border-gray-200 z-10 overflow-hidden pt-2 select-none transition-all duration-300`}
+            onClick={(e) => e.stopPropagation()} ref={modalRef}>
             <div className="overflow-y-auto overflow-x-hidden max-h-96">
-            {conversations && conversations.length === 0 ? (
-					<div className="px-4 pb-2.5 text-sm text-gray-600">
-						Vous n'avez aucun message
-					</div>
-				) : (
-					<ul>
-						{conversations.map((conv) => {
-							const lastMessage = conv.messages[conv.messages.length - 1];
-							const hasUnreadMessages = conv.messages.some(msg => !msg.read && msg.guestId === userId);
-							const otherUser = conv.users.find(user => user._id !== userId);
+                {conversations && conversations.length === 0 ? (
+                    <div className="px-4 pb-2.5 text-sm text-gray-600">
+                        Vous n'avez aucun message
+                    </div>
+                ) : (
+                    <ul>
+                        {conversations.map((conv) => {
+                            const lastMessage = conv.messages[conv.messages.length - 1];
+                            const hasUnreadMessages = conv.messages.some(msg => !msg.read && msg.guestId === userId);
+                            const otherUser = conv.users.find(user => user._id !== userId);
 
-							return (
-								<li
-									key={conv._id}
-									className={`px-4 py-2 text-sm border-b border-gray-100 hover:bg-gray-50 cursor-pointer flex items-center ${
-										hasUnreadMessages ? 'font-bold' : ''
-									}`}
-									onClick={() => onRead(conv._id)}>
-									<div className="flex flex-1 items-center space-x-2 ellipsis">
-										<div className="bg-dark-blue flex h-10 items-center justify-center mx-auto overflow-hidden p-2 relative rounded-full w-10">
-											<span
-												id="avatarLetterNotif"
-												className="text-lg text-white">
-												{
-													otherUser?.username[0]
-												}
-											</span>
-										</div>
-										<div className="flex-1 overflow-hidden">
-											<span className="block truncate">
-												{lastMessage.content}
-											</span>
-											<span className="block text-xs text-gray-400 mt-1 truncate">
-												{formatDateToNow(lastMessage.createdAt)}
-											</span>
-										</div>
-									</div>
-								</li>
-							);
-						})}
-					</ul>
-				)}
+                            return (
+                                <li
+                                    key={conv._id}
+                                    className={`px-4 py-2 text-sm border-b border-gray-100 hover:bg-gray-50 cursor-pointer flex items-center ${
+                                        hasUnreadMessages ? 'font-bold' : ''
+                                    }`}
+                                    onClick={() => onRead(conv._id)}>
+                                    <div className="flex flex-1 items-center space-x-2 ellipsis">
+                                        <div className="bg-dark-blue flex h-10 items-center justify-center mx-auto overflow-hidden p-2 relative rounded-full w-10">
+                                            <span
+                                                id="avatarLetterNotif"
+                                                className="text-lg text-white">
+                                                {otherUser?.username[0]}
+                                            </span>
+                                        </div>
+                                        <div className="flex-1 overflow-hidden">
+                                            <span className="block truncate">
+                                                {lastMessage.content}
+                                            </span>
+                                            <span className="block text-xs text-gray-400 mt-1 truncate">
+                                                {formatDateToNow(lastMessage.createdAt)}
+                                            </span>
+                                        </div>
+                                    </div>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                )}
             </div>
         </div>
     );
