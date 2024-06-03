da905a5cd3b01f8016ae7aba61765f7de81d234a
Thibault <thibault.guilhem@gmail.com>
2024-06-03 12:31:51 +0200

    chore: Add block contact functionality to ModalContact component

--- src/components/ModalContact/BlockContact.jsx
+++ src/components/ModalContact/BlockContact.jsx
@@ -1,11 +1,25 @@
 import React, { useState } from 'react';
+import useBlockContact from '../../api/users/useBlockContact';
 import LoadingDeleteComponent from '../Buttons/LoadingDeleteComponent';
+import { toast } from 'react-toastify';
 
-const BlockContact = () => {
+const BlockContact = ({ closeModal, selectedContact }) => {
+	const blockContact = useBlockContact();
+
     const [isLoading, setIsLoading] = useState(false);
 
-	const handleBlockContact = () => {
-        // Logique pour bloquer le contact
+	const handleBlockContact = async () => {
+        try {
+			setIsLoading(true);
+			await blockContact(selectedContact.id);
+			setIsLoading(false);
+
+			toast.success('Le contact a été retiré avec succès !');
+			closeModal();
+		} catch (error) {
+			toast.error('Échec de la suppression du contact.');
+			return;
+		}
     };
 
     return (
--- src/components/ModalContact/HandleModalContact.jsx
+++ src/components/ModalContact/HandleModalContact.jsx
@@ -136,7 +136,7 @@ const HandleModalContact = ({ closeModal, selectedContact }) => {
 
                 <div className="absolute left-0 top-0 p-4">
                     <div className="w-10 flex justify-between items-center">
-                        {/* <BlockContact /> */}
+                        <BlockContact closeModal={closeModal} selectedContact={selectedContact} />
                         <DeleteContact closeModal={closeModal} selectedContact={selectedContact} />
                     </div>
                 </div>
