fa604e449792676c007b0d5632c9e567214f507c
Thibault <thibault.guilhem@gmail.com>
2024-06-22 21:00:18 +0200

    chore: Update useCreateVisitorSession to include withCredentials option

--- src/api/users/useCreateVisitorSession.jsx
+++ src/api/users/useCreateVisitorSession.jsx
@@ -10,7 +10,11 @@ export const useCreateVisitorSession = () => {
     const createVisitorSession = async () => {
         try {
             const API_URL = process.env.REACT_APP_API_URL;
-            const res = await axios.post(`${API_URL}/users/visitor`);
+            const res = await axios.post(`${API_URL}/users/visitor`, 
+            {},
+            {
+                withCredentials: true,
+            });
             const token = res.data.token;
             document.cookie = `token=${token}`;
             dispatch(setUserId(res.data.tempUser._id));
