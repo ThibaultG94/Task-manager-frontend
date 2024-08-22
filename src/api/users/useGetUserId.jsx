041640eed7c10944ba55f727bfe5f91c971e0a37
Thibault Guilhem <thibault.guilhem@gmail.com>
2024-08-22 11:56:06 +0200

    chore: Update useGetUserId to handle API errors and useErrorApi utility function

--- src/api/users/useGetUserId.jsx
+++ src/api/users/useGetUserId.jsx
@@ -1,12 +1,14 @@
 import { useDispatch, useSelector } from "react-redux";
 import { selectUserId } from "../../store/selectors/userSelectors";
 import { setUserId } from "../../store/feature/users.slice";
+import { useErrorApi } from "../../utils/useErrorApi";
 import axios from "axios";
 
 export const useGetUserId = () => {
     const API_URL = process.env.REACT_APP_API_URL;
     const dispatch = useDispatch();
     const selectedUserId = useSelector(selectUserId);
+    const errorApi = useErrorApi();
 
     const getUserId = async () => {
         let userId;
@@ -14,15 +16,20 @@ export const useGetUserId = () => {
             userId = selectedUserId;
             return selectedUserId;
         } else {
-            const res = await axios.get(`${API_URL}/users/my-account`, {
-                withCredentials: true,
-            });
-            if (res.status !== 200) {
-                throw new Error(res.data);
+            try {
+                const res = await axios.get(`${API_URL}/users/my-account`, {
+                    withCredentials: true,
+                });
+                if (res.status !== 200) {
+                    throw new Error(res.data);
+                }
+                userId = res.data.user._id;
+                dispatch(setUserId(res.data.user._id));
+                return res.data.user._id;
+            } catch (error) {
+                errorApi(error);
             }
-            userId = res.data.user._id;
-            dispatch(setUserId(res.data.user._id));
-            return res.data.user._id;
+          
         }
     };
 
